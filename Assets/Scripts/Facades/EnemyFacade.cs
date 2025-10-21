13591f6c8e887fa86073c591da1debc9fea1f995
frederickylarcher <federicoivanlarcher@gmail.com>
2025-10-21 09:38:56 -0300

    [ADD] Complete EnemyFacade
    Pretty much the same as the PickUpFacade

--- Assets/Scripts/Facades/EnemyFacade.cs
+++ Assets/Scripts/Facades/EnemyFacade.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class EnemyFacade : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private S_SoundSystem soundSystem;
+    private S_AnimationSystem animationSystem;
+    private S_VisualEffectSystem visualEffectSystem;
+
+    public EnemyFacade(S_SoundSystem soundSys, S_AnimationSystem animationSys, S_VisualEffectSystem visualEffectSys)
     {
-        
+        this.soundSystem = soundSys;
+        this.animationSystem = animationSys;
+        this.visualEffectSystem = visualEffectSys;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ExecuteEnemy(string enemyName)
     {
-        
+        soundSystem.PlaySound(enemyName + "_enemy");
+        animationSystem.PlayAnimation(enemyName + "_enemy");
+        visualEffectSystem.PlayEffect(enemyName + "_enemy");
     }
 }
