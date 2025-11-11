289fd42ab8d5521e8f26b4ed6fec40239df5d411
Maurop1243 <mauropari13@gmail.com>
2025-11-11 16:14:11 -0300

    State Machine Funcional
    Arreglé la state machine y el bug del salto, el personaje se mueve como deberia y cambia entre estados como deberia

---
+++ Assets/Scripts/S_Player/States/AirborneState.cs
@@ -0,0 +1,72 @@
+
+using UnityEngine;
+
+public class AirborneState : IState
+{
+    PlayerMovement player;
+    bool isHoldingJump;
+    float groundedGraceTimer;
+
+    public AirborneState(PlayerMovement player)
+    {
+        this.player = player;
+    }
+
+    public void Enter()
+    {
+        Debug.Log("AIRBORNE");
+        // reset vertical then apply impulse
+        player.rb.velocity = new Vector2(player.rb.velocity.x, 0f);
+        player.rb.AddForce(Vector2.up * player.jumpForce, ForceMode2D.Impulse);
+
+        player.jumpTime = 0f;
+        isHoldingJump = true;
+        groundedGraceTimer = 0.1f;
+    }
+
+    public void UpdateState()
+    {
+        // horizontal air control
+        player.rb.velocity = new Vector2(player.inputX * player.moveSpeed, player.rb.velocity.y);
+        ApplyBetterJump();
+
+        if (groundedGraceTimer > 0f)
+            groundedGraceTimer -= Time.deltaTime;
+
+        // variable jump height (hold)
+        if (isHoldingJump && Input.GetButton("Jump"))
+        {
+            player.jumpTime += Time.fixedDeltaTime;
+
+            if (player.jumpTime < player.maxJumpHoldTime && player.rb.velocity.y > 0f)
+            {
+                player.rb.AddForce(Vector2.up * player.jumpHoldForce * Time.fixedDeltaTime, ForceMode2D.Force);
+            }
+            else
+            {
+                isHoldingJump = false;
+            }
+        }
+
+        // Land transitions
+        if (groundedGraceTimer <= 0f && player.IsGrounded())
+        {
+            if (Mathf.Abs(player.inputX) > 0.01f)
+                player.machine.ChangeState(player.machine.runningState);
+            else
+                player.machine.ChangeState(player.machine.idleState);
+        }
+    }
+
+    void ApplyBetterJump()
+    {
+        if (player.rb.velocity.y < 0f)
+        {
+            player.rb.velocity += Vector2.up * Physics2D.gravity.y * (player.fallMultiplier - 1f) * Time.fixedDeltaTime;
+        }
+        else if (player.rb.velocity.y > 0f && !Input.GetButton("Jump"))
+        {
+            player.rb.velocity += Vector2.up * Physics2D.gravity.y * (player.lowJumpMultiplier - 1f) * Time.fixedDeltaTime;
+        }
+    }
+}
--- Assets/Scripts/S_Player/States/StateMachine.cs
+++ Assets/Scripts/S_Player/States/StateMachine.cs
@@ -1,25 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
 
 public class StateMachine : MonoBehaviour
 {
     public IdleState idleState;
     public RunningState runningState;
-    public JumpingState jumpingState;
-    public FallingState fallingState;
+    public AirborneState airborneState; // unified jump+fall
     IState currentState;
+
     public PlayerMovement player;
     public Rigidbody2D rb;
 
-    public void Awake()
+    void Awake()
     {
         player = GetComponent<PlayerMovement>();
-        rb = player.rb;
+        rb = GetComponent<Rigidbody2D>(); // safer than reading player.rb in Awake order
+
         idleState = new IdleState(player);
         runningState = new RunningState(player);
-        jumpingState = new JumpingState(player);  
-        fallingState = new FallingState(player);
+        airborneState = new AirborneState(player);
     }
 
     public void Initialize()
@@ -27,13 +26,16 @@ public class StateMachine : MonoBehaviour
         currentState = idleState;
         currentState.Enter();
     }
+
     public void UpdateState()
     {
         currentState.UpdateState();
     }
+
     public void ChangeState(IState newState)
     {
         currentState = newState;
+        Debug.Log($"? Entering State: {newState.GetType().Name}");
         currentState.Enter();
     }
 }
