c4992a17d68d3476272e534c3aba716844dd8a3f
frederickylarcher <federicoivanlarcher@gmail.com>
2025-11-18 15:35:50 -0300

    [ADD] ItemComponent

---
+++ Assets/Scripts/S_Pickables/S_Items/ItemHerency.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemHerency : MonoBehaviour
+{
+    [SerializeField] public string ItemName;
+    [SerializeField] public int ItemPrice;
+}
